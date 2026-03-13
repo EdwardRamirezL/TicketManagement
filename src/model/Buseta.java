fb37f4fa80337bae08163da61a43057424d626ce
Dayanna Mendoza <dayannammendoza@unicesar.edu.co>
2026-03-13 00:16:30 -0500

    feat: Subclase buseta, sus constructores y metodos

--- src/model/Buseta.java
+++ src/model/Buseta.java
@@ -8,6 +8,26 @@ package model;
  *
  * @author ASUS
  */
-public class Buseta {
-    
+
+public class Buseta extends Vehiculo {
+
+    public Buseta(String placa, String ruta) {
+        super(placa, ruta);
+        this.capacidadMaxima = 19;
+        this.tarifaBase = 8000;
+    }
+
+    @Override
+    public void imprimirDetalle() {
+
+        System.out.println("Tipo: Buseta");
+        System.out.println("Placa: " + placa);
+        System.out.println("Ruta: " + ruta);
+        System.out.println("Capacidad: " + capacidadMaxima);
+        System.out.println("Cupos disponibles: " + cuposDisponibles());
+        System.out.println("Tarifa: " + tarifaBase);
+
+    }
+
 }
+
