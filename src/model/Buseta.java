f5a913902e518127591748491fe7a87b406a10ff
Dayanna Mendoza <dayannammendoza@unicesar.edu.co>
2026-03-13 00:10:48 -0500

    feat: Clase vehiculo con setters y getters

--- src/model/Buseta.java
+++ src/model/Buseta.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model;
 
@@ -8,7 +8,6 @@ package model;
  *
  * @author ASUS
  */
-public interface Imprimible {
-     void imprimirDetalle();
+public class Buseta {
     
 }
