628f2864fc02e957d107c6d274a15e183f286a5f
Alejandro Arias Cala <ariascalaalejandro@gmail.com>
2026-03-12 22:24:19 -0500

    feat: Implementar calcularDescuento en la clase PasajeroEstudiante

--- src/model/PasajeroEstudiante.java
+++ src/model/PasajeroEstudiante.java
@@ -13,6 +13,11 @@ public class PasajeroEstudiante extends Pasajero {
     public PasajeroEstudiante(String cedula, String nombre) {
         super(cedula, nombre);
     }
+
+    @Override
+    public double calcularDescuento() {
+        return 0.15;
+    }
     
     
 }
