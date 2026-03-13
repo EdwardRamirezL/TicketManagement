952e796937dd0d094b5786d6dbc8f079cc32af55
Alejandro Arias Cala <ariascalaalejandro@gmail.com>
2026-03-12 21:18:08 -0500

    feat: Creacion de la clase Conductor, implementacion de herencia y atributos

---
+++ src/Service/Conductor.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Service;
+
+/**
+ *
+ * @author dimas
+ */
+public class Conductor extends Persona {
+    private String numeroLicencia; 
+    private String categoriaLicencia;
+}
