b653e9b28589e3ed7f7dd26747c80f2d8d4ea658
Alejandro Arias Cala <ariascalaalejandro@gmail.com>
2026-03-18 02:40:31 -0500

    feat: Implementar metodo guardar a PasajeroDAO

--- src/dao/PasajeroDAO.java
+++ src/dao/PasajeroDAO.java
@@ -8,6 +8,26 @@ package dao;
  *
  * @author dimas
  */
+import model.Pasajero;
+import model.PasajeroRegular;
+import model.PasajeroEstudiante;
+import model.PasajeroAdultoMayor;
+
+import java.io.*;
+import java.util.ArrayList;
+
+
 public class PasajeroDAO {
     private final String archivo = "pasajeros.txt";
+
+
+public void guardar(Pasajero p){
+try(BufferedWriter bw = new BufferedWriter(new FileWriter(archivo, true))){
+  String tipo = p.getClass().getSimpleName();
+  bw.write(p.getCedula() + ";" + p.getNombre() + ";" + tipo); 
+  bw.newLine();
+}catch(IOException e){
+  e.printStackTrace();
+}
+}
 }
