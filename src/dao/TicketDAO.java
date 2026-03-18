f22eca380387b0f84ab700a678228de4feba9be9
Alejandro Arias Cala <ariascalaalejandro@gmail.com>
2026-03-18 03:04:24 -0500

    feat: Implementacion metodo guardar a la clase TicketDAO

--- src/dao/TicketDAO.java
+++ src/dao/TicketDAO.java
@@ -15,4 +15,20 @@ import java.util.ArrayList;
 
 public class TicketDAO {
     private final String archivo = "tickets.txt";
+    
+    public void guardar(Ticket t){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(archivo, true))){
+            bw.write(t.getId() + ";" + 
+                    t.getPasajero().getCedula()+ ";" +
+                    t.getVehiculo().getPlate() + ";" +
+                    t.getFechaHora() + ";" + 
+                    t.getOrigen() + ";" + 
+                    t.getDestino() + ";" + 
+                    t.calcularTotal());
+            bw.newLine();
+            
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
