240f8df96e7361b5e81bdfee39ee3bb788abb4c4
Alejandro Arias Cala <ariascalaalejandro@gmail.com>
2026-03-18 02:56:55 -0500

    feat: Creacion de la clase TicketDAO con atributo archivo

---
+++ src/dao/TicketDAO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author dimas
+ */
+import model.Ticket;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class TicketDAO {
+    private final String archivo = "tickets.txt";
+}
