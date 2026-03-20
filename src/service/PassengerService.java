965535271055d5ee410e6ca9d890006296d1ba1f
Alejandro Arias Cala <ariascalaalejandro@gmail.com>
2026-03-19 19:36:43 -0500

    feat: Creacion de la clase PassengerService

---
+++ src/service/PassengerService.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+
+/**
+ *
+ * @author dimas
+ */
+import dao.PassengerDAO;
+import model.Passenger;
+
+import java.util.List;
+
+public class PassengerService {
+    private PassengerDAO dao = new PassengerDAO(); 
+}
