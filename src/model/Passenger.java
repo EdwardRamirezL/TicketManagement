d81321786fdc16e824e5fd79f31bf0a53488c2bb
Dayanna Mendoza <dayannammendoza@unicesar.edu.co>
2026-03-21 06:28:53 -0500

    feat:Modificaciones a la clase passanger (ahora la edad se calcula con la fecha de nacimiento)

--- src/model/Passenger.java
+++ src/model/Passenger.java
@@ -3,15 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model;
-
+import java.time.LocalDate;
+import java.time.Period;
 /**
  *
  * @author dimas
  */
 public abstract class Passenger extends Person {
+    
+    protected LocalDate birthDate;
+    protected double discount;
 
-    public Passenger(String id, String name) {
+    public Passenger(String id, String name, LocalDate birthDate) {
         super(id, name);
+        this.birthDate = birthDate;
+    }
+    
+    public boolean isSenior(){
+
+        int age = Period.between(birthDate, LocalDate.now()).getYears();
+        return age >= 60;
+
     }
     
     public abstract double calculateDiscount();
